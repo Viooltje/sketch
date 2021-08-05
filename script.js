0949bb51a63c2a7608f7e75d6abde5aaec7ee8f5
viooltje <chantal.houtschild@outlook.com>
2021-08-05 20:13:09 +0200

    Added basic CSS made a start on JS grid function and eventlistener

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
-// Show the value of the range value 
+// Get value to work globaly 
+// fix makeGrid
+// More comments
+
+// Selecting dom elements 
+const gridContainer = document.querySelectorAll('.container');
 const slider = document.getElementById('number-of-grid');
+
+// Variable to update 
+let value;
+
+// Eventlisteners maybe
+
+
+// Show the value of the range value 
 let output = document.querySelector('.new-value');
 output.innerHTML = `${slider.value} x ${slider.value}`;
-
-let value; 
 
 slider.oninput = function() {
     value = this.value;
     output.innerHTML = `${value} x ${value}`;
+    return value;
+} 
+
+slider.addEventListener('mouseup', makeGrid)
+// Create the grid
+function makeGrid(){
+    for (i = 0; i < value; i++) {
+    console.log('hoi');
+    var grid = document.createElement('div');
+    grid.className = 'grid';
+    gridContainer.appendChild('grid');
+    }
+
 }
 
-// Create the grid
+
 
 
 
